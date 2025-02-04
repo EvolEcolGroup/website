69b51f302163e89fbf528e7466c775b7661a665d
Andrea Manica <am315@cam.ac.uk>
2025-02-04 14:56:43 +0000

    Update template with new matadata

---
+++ r_scripts/update_gscholar_downloads.R
@@ -0,0 +1,29 @@
+# get a list of qmd files from the people directory
+qmd_files <- list.files("people", pattern = ".qmd$", full.names = TRUE)
+# remove the template file
+qmd_files <- qmd_files[-grep("template", qmd_files)]
+# create a data.frame to store the information
+qmd_data <- data.frame(file_csv = paste0(tools::file_path_sans_ext(qmd_files),"_pubs.csv"),
+                       gscholar_id = rep(NA, length(qmd_files)),
+                       eeg_start = rep(NA, length(qmd_files)),
+                       papers_name =rep(NA, length(qmd_files)))
+# loop over the files and extract the appropriate metadata
+for (i in seq_len(qmd_files)){
+  # read the file
+  lines <- readLines(qmd_files[i])
+  get_meta_field <- function(field, lines){
+    line_with_meta <- grep(field, lines)
+    if (length(line_with_meta) == 0) {return(NA)}
+    field <- lines[grep(field, lines)]
+    field <- gsub(paste0(field, ": "), "", field)
+    field <- gsub('.*"(.*)".*', '\\1', field)
+    return(field)
+  }
+  # get the gscholar_id
+  qmd_data$gscholar_id[i] <- get_meta_field("gscholar_id", lines)
+  qmd_data$eeg_start[i] <- get_meta_field("eeg_start", lines)
+  qmd_data$papers_name[i] <- get_meta_field("papers_name", lines)
+}
+
+# update the csv files
+
