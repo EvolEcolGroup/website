afca77e4ba8b4afcc2a9641b818749873f62f91d
Andrea Manica <am315@cam.ac.uk>
2025-02-09 21:48:22 +0000

    add word cloud

--- r_scripts/title_word_cloud.R
+++ r_scripts/title_word_cloud.R
@@ -1,43 +1,50 @@
 # create a word cloud from publication titles
-# using https://semba-blog.netlify.app/11/05/2019/wordclouds-plotting-with-ggwordcloud-package-in-r/
-# as a template
-library(ggwordcloud)
-library(tm)
-pubs <- read.csv("./people/andrea_manica_pubs.csv")
+# following: https://remybeugnon.netlify.app/post/extract-my-scientific-profile-with-r/
 
-ggplot(pubs, aes(label = title)) +
-  geom_text_wordcloud() +
-  theme_minimal()
+pub <- read.csv("./people/andrea_manica_pubs.csv")
 
-# create a corpus object
-pubs_corpus = pubs$title  %>% 
-  tm::VectorSource()%>% 
-  tm::Corpus()
+library("tm")
+library("SnowballC")
+library("wordcloud2")
+library(dplyr)
 
-# clean it up
-pubs_corpus_clean =  pubs_corpus %>% 
-  tm_map(FUN = content_transformer(tolower)) %>% # Convert the text to lower case
-  tm_map(FUN = removeNumbers) %>% # Remove numbers
-  tm_map(removeWords, stopwords("english")) %>% # Remove english common stopwords
-#  tm_map(removeWords, c("will", "let", "ring")) %>%   # Remove words 
-  tm_map(removePunctuation) %>%   # Remove punctuations
-  tm_map(stripWhitespace)   # Eliminate extra white spaces
+# List of words to remove
+list.stop = c('affect', 'effects', 'using')
 
-# create word frequency table (this needs fixing, as it uses the first column as freq, which is not)
-# We need to compute rowsums for the matrix that we obtain from the TermDocumentMatrix
-# TODO fix bug
-pubs_corpus_clean_tb=  pubs_corpus_clean %>% 
-  tm::TermDocumentMatrix() %>% 
-  as.matrix() %>% as.data.frame() %>% 
-  tibble::rownames_to_column() %>%
-  dplyr::rename(word = 1, freq = 2) %>%
-  dplyr::arrange(desc(freq))
+docs <- Corpus(VectorSource(pub$title))
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- docs %>%
+  tm_map(., toSpace, "/") %>%
+  tm_map(., toSpace, "@") %>%
+  tm_map(., toSpace, "\\|") %>%
+  tm_map(., content_transformer(tolower)) %>%
+  tm_map(., removeNumbers) %>%
+  tm_map(., removeWords, stopwords("english")) %>%
+  tm_map(., removeWords, list.stop) %>%
+  tm_map(., removePunctuation) %>%
+  tm_map(., stripWhitespace)
 
-# And now plot it
-ggplot(data = pubs_corpus_clean_tb, 
-       aes(label = word, size = freq, col = as.character(freq))) + 
-  geom_text_wordcloud(rm_outside = TRUE, max_steps = 1,
-                      grid_size = 1, eccentricity = .9)+
-  scale_size_area(max_size = 14)+
-  scale_color_brewer(palette = "Paired", direction = -1)+
-  theme_void()
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+col.pal = colorRampPalette(colors = c('brown','Darkgreen'))
+
+# plot it with worldcloud2
+wordcloud2(data = d, 
+           size = 1, 
+           color = col.pal(10))
+
+# # And now plot it
+# library(ggwordcloud)
+# 
+# ggplot(data = d, 
+#        aes(label = word, size = freq, col = as.character(freq))) + 
+#   geom_text_wordcloud(rm_outside = TRUE, max_steps = 1,
+#                       grid_size = 1, eccentricity = .9)+
+#   scale_size_area(max_size = 14)+
+#   scale_color_brewer(palette = "Paired", direction = -1)+
+#   theme_void()
+
